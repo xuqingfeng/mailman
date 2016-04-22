53ee58895cfe32e7a78cab5f6d0267488fc88ebf
xuqingfeng <js-xqf@hotmail.com>
2016-04-22 22:31:43 +0800

    test: lang.go

--- lang/lang_test.go
+++ lang/lang_test.go
@@ -1,3 +1,21 @@
 package lang
 
+import "testing"
 
+func TestSaveLang(t *testing.T) {
+
+	lang := Lang{"zh"}
+	err := SaveLang(lang)
+	if err != nil {
+		t.Errorf("SaveLang() fail %v", err)
+	}
+}
+
+func TestGetLang(t *testing.T) {
+
+	lng, err := GetLang()
+	if err != nil {
+		t.Errorf("GetLang() fail %v", err)
+	}
+	t.Log(lng)
+}
